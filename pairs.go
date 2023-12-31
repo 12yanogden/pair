78b6567c324d463ad19bc09548ca546a7833e6f1
Ryan Ogden <12yanogden@gmail.com>
2023-12-31 14:05:36 -0700

    add String(), Deconstruct()

--- pairs.go
+++ pairs.go
@@ -1,10 +1,23 @@
 package pairs
 
+import "fmt"
+
 type Pair[F comparable, S comparable] struct {
 	First  F
 	Second S
 }
 
+// Return true if the pair given is equal to the pair called. Else, false.
 func (p1 Pair[F, S]) Equals(p2 Pair[F, S]) bool {
 	return p1.First == p2.First && p1.Second == p2.Second
+}
+
+// Return values in pair
+func (p Pair[F, S]) Deconstruct() (F, S) {
+	return p.First, p.Second
+}
+
+// Return string representation of pair
+func (p Pair[F, S]) String() string {
+	return fmt.Sprintf("{%v, %v}", p.First, p.Second)
 }
--- pairs_test.go
+++ pairs_test.go
@@ -23,3 +23,23 @@ func TestNotEquals(t *testing.T) {
 		t.Fatalf("\nExpected\t%t, the pairs are NOT equal\nActual:\t\t%t, the pairs are equal", expected, actual)
 	}
 }
+
+func TestDeconstruct(t *testing.T) {
+	p := Pair[string, string]{"a", "b"}
+	expected1, expected2 := "a", "b"
+	actual1, actual2 := p.Deconstruct()
+
+	if expected1 != actual1 || expected2 != actual2 {
+		t.Fatalf("\nExpected:\t%s, %s\nActual:\t\t%s, ,%s", expected1, expected2, actual1, actual2)
+	}
+}
+
+func TestString(t *testing.T) {
+	p := Pair[string, string]{"a", "b"}
+	expected := "{a, b}"
+	actual := p.String()
+
+	if expected != actual {
+		t.Fatalf("\nExpected:\t%s\nActual:\t\t%s", expected, actual)
+	}
+}
